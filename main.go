d0b194c116fe7134343d128b25a7178ce67de1c3
logandavies181 <logan.davies181@gmail.com>
2022-12-21 08:43:41 +0000

    walk the tree instead of substing it raw

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/drone/envsubst"
@@ -10,25 +11,75 @@ import (
 	"sigs.k8s.io/kustomize/kyaml/yaml"
 )
 
+func walkSequenceNode(in *yaml.RNode) error {
+	_, err := filter(in)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func walkMapNode(in *yaml.MapNode) error {
+	_, err := filter(in.Value)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func filter(in *yaml.RNode) (*yaml.RNode, error) {
-	str, err := in.String()
-	if err != nil {
-		return nil, err
+	if in.IsNil() {
+		return nil, nil
 	}
 
-	substed, err := envsubst.EvalEnv(str)
-	if err != nil {
-		return nil, err
+	switch y := in.YNode().Kind; y {
+	case yaml.MappingNode:
+		err := in.VisitFields(walkMapNode)
+		if err != nil {
+			return nil, fmt.Errorf("Could not visit fields: %v", err)
+		}
+		return in, nil
+	case yaml.SequenceNode:
+		err := in.VisitElements(walkSequenceNode)
+		if err != nil {
+			return nil, fmt.Errorf("Could not visit elements: %v", err)
+		}
+
+		return in, nil
+	case yaml.ScalarNode:
+		str, err := in.String()
+		if err != nil {
+			return nil, fmt.Errorf("Could not parse node into string: %v", err)
+		}
+
+		substed, err := envsubst.EvalEnv(str)
+		if err != nil {
+			return nil, fmt.Errorf("Could not envsubt: %v", err)
+		}
+
+		if substed == str {
+			return in, nil
+		}
+
+		strNode, err := yaml.Parse(substed)
+		if err != nil {
+			return nil, fmt.Errorf("Could not parse node after envsubsting: %v", err)
+		}
+
+		out, err := in.Pipe(yaml.Set(strNode))
+		if err != nil {
+			return nil, err
+		}
+
+		return out, nil
+
+	case yaml.AliasNode, yaml.DocumentNode:
+		fallthrough
+	default:
+		panic(fmt.Sprintf("Unknown Kind: %v", y))
 	}
-
-	obj, err := yaml.Parse(substed)
-	if err != nil {
-		return nil, err
-	}
-
-	*in = *obj
-
-	return in, nil
 }
 
 func main() {
@@ -36,7 +87,7 @@ func main() {
 		for i := range items {
 			err := items[i].PipeE(yaml.FilterFunc(filter))
 			if err != nil {
-				return nil, err
+				return nil, fmt.Errorf("kustomize-krm-envsubt: %v", err)
 			}
 		}
 		return items, nil
